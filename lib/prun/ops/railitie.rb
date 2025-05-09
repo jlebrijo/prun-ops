1d9a93e4a6d4975783503e25489f58607d17fe35
jlebrijo <jlebrijo@gmail.com>
2025-05-09 23:54:26 +0200

    Fix rake tasks

--- lib/prun/ops/railitie.rb
+++ lib/prun/ops/railitie.rb
@@ -5,7 +5,7 @@ require "rails"
 module PrunOps
   class Railtie < Rails::Railtie
     rake_tasks do
-      Dir.glob("#{File.dirname(__FILE__)}/../tasks/*.rake").each { |r| load r }
+      Dir.glob("#{File.dirname(__FILE__)}/../../tasks/*.rake").each { |r| load r }
     end
   end
 end
